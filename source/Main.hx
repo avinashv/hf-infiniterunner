842a3acb653a309d6cd22315d40db6393fe91ad0
Avinash Vora <avinashvora@gmail.com>
2020-04-27 16:02:49 +0400

    Instantiated project to mobile screensize.

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,20 @@
 package;
 
 import flixel.FlxGame;
+import flixel.FlxState;
 import openfl.display.Sprite;
 
-class Main extends Sprite
-{
-	public function new()
-	{
-		super();
-		addChild(new FlxGame(0, 0, PlayState));
-	}
+class Main extends Sprite {
+    var gameWidth:Int = 375;
+    var gameHeight:Int = 812;
+    var initialState:Class<FlxState> = PlayState;
+    var zoom:Float = 1;
+    var framerate:Int = 60;
+    var skipSplash:Bool = true;
+    var startFullscreen:Bool = false;
+    
+    public function new() {
+        super();
+        addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+    }
 }
